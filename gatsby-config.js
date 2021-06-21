4b1b53b78d4769a1738040c8ce56820de0c73105
Roscoe Hart <roscoewh@gmail.com>
2021-06-21 21:34:43 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: "GeoTiles",
+  },
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
