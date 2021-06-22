637f763f69c232a0572c92f179ffe23c4b4aa4aa
Roscoe Hart <roscoewh@gmail.com>
2021-06-22 19:44:05 +1000

    Customise theme

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,12 +4,12 @@ import { jsx } from "theme-ui";
 import { useConfig } from "../hooks/useConfig";
 import Header from "./Header";
 
-export default function Layout({ title, children }) {
+export default function Layout({ children }) {
   const { title } = useConfig();
   return (
     <div>
       <Header title={title} />
-      <main sx={{ maxWidth: 420, m: "0 auto", px: 2 }}>{children}</main>
+      <main sx={{ maxWidth: 9, m: "0 auto", px: 3 }}>{children}</main>
     </div>
   );
 }
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,218 @@
+import "typeface-rubik";
+
+const theme = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512, 640, 720, 960],
+  sizes: [0, 4, 8, 16, 32, 64, 128, 256, 512, 640, 720, 960],
+  radii: [0, 2, 4, 8, 16, 32, 64, 99999],
+  breakpoints: ["16em", "24em", "32em", "40em", "48em", "64em"],
+  colors: {
+    light: "#ffffff",
+    dark: "#000000",
+    text: "#333333",
+    background: "#ffffff",
+    primary: "#ffc6ff",
+    secondary: "#9bf6ff",
+    muted: "rgba(255, 255, 255, 0.05)",
+  },
+  fonts: {
+    body: '"Rubik", system-ui, -apple-system, sans-serif',
+    heading: '"Rubik", system-ui, -apple-system, sans-serif',
+    monospace: "monospace",
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    default: "1px solid",
+  },
+  styles: {
+    root: {
+      fontFamily: "body",
+      lineHeight: "body",
+      fontWeight: "body",
+    },
+    h1: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "body",
+      fontSize: 7,
+    },
+    h2: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 4,
+      mb: 3,
+    },
+    h3: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 3,
+    },
+    h4: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 2,
+    },
+    h5: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 1,
+    },
+    h6: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 0,
+    },
+    p: {
+      color: "text",
+      fontFamily: "body",
+      fontWeight: "body",
+      lineHeight: "body",
+    },
+    a: {
+      color: "primary",
+      textDecoration: "none",
+      boxShadow: (t) => `inset 0 -1px 0 ${t.colors.primary}`,
+    },
+    hr: {
+      border: "none",
+      py: 3,
+    },
+  },
+  text: {
+    small: {
+      fontSize: 1,
+    },
+  },
+  buttons: {
+    primary: {
+      display: "inline-flex",
+      justifyContent: "center",
+      alignItems: "center",
+      bg: "transparent",
+      color: "primary",
+      border: "default",
+      py: 1,
+      px: 2,
+      textDecoration: "none",
+      borderRadius: 3,
+      "&:hover": {
+        color: "secondary",
+      },
+      "&:active": {
+        color: "light",
+      },
+      "&:disabled": {
+        color: "spectrum.orange",
+      },
+      "& > *": {
+        mr: 1,
+      },
+    },
+  },
+  links: {
+    heading: {
+      color: "primary",
+      fontFamily: "heading",
+      fontSize: 4,
+      textDecoration: "none",
+      boxShadow: (t) => `inset 0 -2px 0 ${t.colors.primary}`,
+      "&:hover": {
+        color: "secondary",
+        boxShadow: (t) => `inset 0 -2px 0 ${t.colors.secondary}`,
+      },
+      "&:active": {
+        color: "light",
+        boxShadow: (t) => `inset 0 -2px 0 ${t.colors.light}`,
+      },
+    },
+    title: {
+      color: "primary",
+      fontFamily: "heading",
+      fontSize: 3,
+      textDecoration: "none",
+      "&:hover": {
+        color: "secondary",
+      },
+      "&:active": {
+        color: "light",
+      },
+    },
+    button: {
+      display: "inline-flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "primary",
+      border: "default",
+      py: 1,
+      px: 2,
+      textDecoration: "none",
+      borderRadius: 3,
+      "&:hover": {
+        color: "secondary",
+      },
+      "&:active": {
+        color: "light",
+      },
+      "& > *": {
+        mr: 1,
+      },
+    },
+  },
+  cards: {
+    primary: {
+      bg: "muted",
+      p: "3",
+      borderRadius: 4,
+    },
+  },
+  forms: {
+    input: {
+      outline: "none",
+      bg: "muted",
+      border: "none",
+      borderRadius: 3,
+      mb: 2,
+      "&:hover": {
+        boxShadow: (t) => `inset 0 0 0 1px ${t.colors.secondary}`,
+      },
+      "&:focus": {
+        boxShadow: (t) => `inset 0 0 0 1px ${t.colors.light}`,
+      },
+    },
+    textarea: {
+      fontFamily: "body",
+      outline: "none",
+      bg: "muted",
+      border: "none",
+      borderRadius: 3,
+      mb: 2,
+      "&:hover": {
+        boxShadow: (t) => `inset 0 0 0 1px ${t.colors.secondary}`,
+      },
+      "&:focus": {
+        boxShadow: (t) => `inset 0 0 0 1px ${t.colors.light}`,
+      },
+    },
+  },
+};
+
+export default theme;
